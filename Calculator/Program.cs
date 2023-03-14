22e19a49ccc1e07022fa3ee4619790e03d5b931c
tim finholm <timfinholm@hotmail.com>
2023-03-13 20:06:44 -0400

    add to integers

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
