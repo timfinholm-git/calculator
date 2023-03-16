b8d74a0e69f34959117660a1f79fa7353dbcc10a
tim finholm <timfinholm@hotmail.com>
2023-03-15 20:48:08 -0400

    added input type checking

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,49 +1,106 @@
-﻿namespace calculator
+﻿using calculator;
+using System;
+
+namespace calculator
 {
-    internal class Program
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            float num1 = 0;
-            float num2 = 0;
+            bool endApp = false;
 
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
-            Console.WriteLine("Type a number, then press Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Type another number, and then press Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Choose an option from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            switch (Console.ReadLine()) 
+            
+            while (!endApp)
             {
-                case "a":
-                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-                    break;
-                case "s":
-                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-                    break;
-                case "m":
-                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-                    break;
-                case "d":
-                    while (num2 ==0)
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+                Console.Write("Type a number, then press Enter: ");
+                numInput1 = Console.ReadLine();
+                double cleanNum1 = 0;
+                bool isNumber1 = double.TryParse(numInput1, out cleanNum1);
+                while (isNumber1 != true)
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput1 = Console.ReadLine();
+                    isNumber1 = double.TryParse(numInput1, out cleanNum1);
+                }
+                
+                Console.Write("Type another number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+                double cleanNum2 = 0;
+                bool isNumber2 = double.TryParse(numInput2, out cleanNum2);
+                while (isNumber2 != true)
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput2 = Console.ReadLine();
+                    isNumber2 = double.TryParse(numInput2, out cleanNum2);
+                }
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option? ");
+
+                string op = Console.ReadLine();
+
+                try 
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
                     {
-                        Console.WriteLine("Cannot divide by 0, please enter any number other than 0: ");
-                        num2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+
                     }
-                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-                    break;
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch(Exception e) 
+                {
+                    Console.WriteLine("An exception occorred while trying to do the math.\n - Details: " + e.Message);
+                }
+                Console.Write("Press 'n' and Enter to close the calculator console app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n")
+                {
+                    endApp = true;
+                }
+                Console.WriteLine("\n");
+
+
             }
-            Console.Write("Press any key to close the calculator console app...");
-            Console.ReadKey();
+            return;
         }
     }
 }
