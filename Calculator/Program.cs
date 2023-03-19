68606f4779f48d46dac3c25b624cb0691b8e55e5
tim finholm <timfinholm@hotmail.com>
2023-03-19 17:23:23 -0400

    separated library from program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,38 +1,9 @@
-﻿using calculator;
+﻿using CalculatorProgram;
+using CalculatorLibrary;
 using System;
 
-namespace calculator
+namespace CalculatorProgram
 {
-    class Calculator
-    {
-        public static double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN;
-
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    break;
-                case "d":
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            return result;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
